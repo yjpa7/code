0cafd3c5f2a89e3995113208488f28f4c1d7efb6
yjpa7 <yjpa7@163.com>
2017-10-10 18:02:22 +0800

    添加完整bootstrap-3.3.7-dist文件

---
+++ xinhu_utf8_v1.5.9/webmain/main/kaoqinj/rock_kaoqinj_Udcrbb.php
@@ -0,0 +1,22 @@
+
+
+//订餐日报表
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<link rel="stylesheet" type="text/css" href="mode/bootstrap3.3/css/bootstrap.min.css"/>
+	<script type="text/javascript" src="web/res/js/jquery.1.9.1.min.js"></script>
+</head>
+<body>
+
+	sdfsdf<br>
+	测试
+	<button type="button" class="btn btn-default" aria-label="Left Align">
+	  <span class="glyphicon glyphicon-align-left" aria-hidden="true">Star</span>
+	</button>
+	<span class="glyphicon glyphicon-asterisk" aria-hidden="true"></span>
+    <span class="glyphicon-class">glyphicon glyphicon-asterisk</span>
+
+</body>
+</html>
